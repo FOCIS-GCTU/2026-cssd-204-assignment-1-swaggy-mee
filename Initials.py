d2217a8eb3285e0b754365dee4f0cca38d3fb0a4
swaggy-mee <ha3670166@gmail.com>
2026-06-01 00:36:31 +0000

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,49 @@
+"""
+Program Description: This program prints the initials 'HAH' in stylized large letters,
+                     where each large letter is made up of its own capital letter.
+                     Since there are only two initials, the letter 'H' is used
+                     as a stand-in for the third initial (first initial repeated).
+                     The output includes small initials, then large block letters,
+                     each 12 chars wide x 10 chars high, followed by a period made of 4 asterisks.
+                     Three columns of periods appear on both sides of each letter/period,
+                     except the rightmost period. Total output is 10 lines high, 60 chars wide.
+Author: HUDAL ADAMS
+Date: 31/5/2025
+Course: scripting languagges
+Assignment: 1
+"""
+
+
+
+def main():
+    # Prints the user's initials 'HAH' in small and large stylized format
+    # Small initials line
+    print()
+    print("...HAH")
+    print()
+    
+    # Line 1
+    print("...H   H   AAA   H   H....")
+    # Line 2
+    print("...H   H  A   A  H   H....")
+    # Line 3
+    print("...H   H  A   A  H   H....")
+    # Line 4
+    print("...HHHHH  AAAAA  HHHHH....")
+    # Line 5
+    print("...H   H  A   A  H   H....")
+    # Line 6
+    print("...H   H  A   A  H   H....")
+    # Line 7
+    print("...H   H  A   A  H   H....")
+    # Line 8
+    print("...H   H  A   A  H   H....")
+    # Line 9
+    print("...H   H  A   A  H   H....")
+    # Line 10
+    print("...H   H  A   A  H   H....")
+    
+    # Final blank line after large initials
+    print()
+
+
